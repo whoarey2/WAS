9f090c234749efa8c6b961ee0bb5e8bcb5a83bc7
val4rs7ark <val4rs7ark@gmail.com>
2019-08-27 18:30:32 +0900

    2019-08-27 김시성

---
+++ workspace_was/was/src/main/java/com/was/erp/ErpController.java
@@ -0,0 +1,19 @@
+package com.was.erp;
+
+
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value="/aqua")
+@Controller
+public class ErpController {
+	Logger logger = Logger.getLogger(ErpController.class);
+	@GetMapping("login.was")
+	public String erpLogin() {
+		logger.info("WAS의 시작!!!!!");
+		return"";
+	}
+}
